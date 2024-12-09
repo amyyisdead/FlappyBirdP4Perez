33ac9d455117db8cf7aabc27090d4b70b1fcaa00
amyyisdead <754989@student.dusd.net>
2024-12-09 12:01:24 -0800

    moving background
    Today i made my flappy bird game background scroll to give it the effect the bird was flying forward.

--- P4FinalPerez/Assets/Scripts/Bird.cs
+++ P4FinalPerez/Assets/Scripts/Bird.cs
@@ -32,9 +32,11 @@ public class Bird : MonoBehaviour
 
         }
     }
-    private void OnCollisionEnter2D ()
+    void OnCollisionEnter2D ()
     {
+        rb2d.velocity = Vector2.zero;
         isDead = true;
         anim.SetTrigger("Die");
+        GameControl.instance.BirdDied();
     }
 }
